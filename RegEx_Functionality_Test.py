4820e597bd7efd4485f8db649133ed4afe60708a
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-10-05 11:00:01 -0400

    RegEx Price finder works

--- RegEx_Functionality_Test.py
+++ RegEx_Functionality_Test.py
@@ -1,10 +1,13 @@
-import unittest
+import re
+import requests
 
+txt = "johann.33es 2812%^@ there $ is ch.icken i$11.11.11nside of my h3art  the price is $10,49."
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+sub_test_txt_a = "the price is:  $1042.43 dadsfaaf"
 
+sub_test_txt_b = "the price is:  $ten forty three"
 
-if __name__ == '__main__':
-    unittest.main()
+found = re.search(r"\B\$\d+((\.|,)\d{2})", txt)
+where = found.span()
+print(where)
+print(txt[where[0]: where[1]])
