1bb59b24e23b832d59e263e0db4bbe1d2af3c00e
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-10-10 08:10:21 -0400

    RegEx test and functionality has been understood.  Product title algorithm is next

--- Test_Files/chewy_scrape_test.py
+++ Test_Files/chewy_scrape_test.py
@@ -2,6 +2,14 @@ import re
 import requests
 import bs4
 
+"""
+
+This block of code is for live scrapping however the html on "https://www.chewy.com/b/pharmacy-2515" has already been
+stored inside (test_html/chewy_pharmacy_test.html).  This allows us to create a scraping algorithm without constantly
+sending GET requests to "chewy.com";  Constant requests get the ip blacklisted.
+
+----------------------------------------------------
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko)'
                   'SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36'
@@ -11,18 +19,27 @@ headers = {
 website = "https://www.chewy.com/b/pharmacy-2515"
 webtxt = requests.get(website, headers=headers).text
 
-# try:
-#     with open("test_html/chewy_pharmacy_test.html", "w") as html_file:
-#         html_file.write(webtxt)
-# except Exception as e:
-#     print("unsuccesful")
-#     print(e)
+try:
+    with open("test_html/chewy_pharmacy_test.html", "w") as html_file:
+        html_file.write(webtxt)
+except Exception as e:
+    print("unsuccesful")
+    print(e)
+    
+----------------------------------------------------
+
+"""
+
+with open("test_html/chewy_pharmacy_test.html", "r") as html_file:
+    webtxt = html_file.read()
 
 
-all_found = re.finditer(r"[$]\d+\.\d{2}", webtxt)
-found = re.search(r"\B\$\d+([.,]\d{2})", webtxt)
+# all_found = re.finditer(r"[$]\d+\.\d{2}", webtxt)
+all_foundb = re.finditer(r"<div>(<\w+>\w+</\w+>)?\W+([a-zA-Z0-9]+|\s|[()\"\',-])+", webtxt)
+# for _ in all_found:
+#     print(_)
 
-print(found.group() + "\n", end="\n")
+print(webtxt[588456:588490])
 
-for _ in all_found:
+for _ in all_foundb:
     print(_)
