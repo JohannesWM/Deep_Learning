e236108784186cf5afd7d70e532f56e6a92752c7
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-10-14 12:55:43 -0400

    getting started with Consumer_Pages_to_HTML.py

--- Training_Data/python_files/Consumer_Pages_to_HTML.py
+++ Training_Data/python_files/Consumer_Pages_to_HTML.py
@@ -14,8 +14,13 @@ Inputs for universal fuctions may include but are not limited too:  specific lin
 formatting styles...
 '''
 
+Warning_Text = "\033[4m\033[1m\033[93m"
 
+
+# to be completed later current bugs include request.get() differing from website html and therefore scraping for proper
+# links is near impossible
 def scrape_ASOS():
+    print(f"{Warning_Text}This function has not been completed: scrape_ASOS()")
     with open("../html_files/asos.html", "r") as asos_file:
         asos_as_txt = asos_file.read()
         a_tags = re.finditer("<a.*?>.*?</a>", asos_as_txt)
@@ -23,6 +28,3 @@ def scrape_ASOS():
     a_tags_list = [asos_as_txt[_.span()[0]:_.span()[1]] for _ in a_tags]
     WM_page_list = [i for i in a_tags_list if re.search("id=\"women-floor\">WOMEN<", i) is not None or
                     re.search("id=\"men-floor\">MEN<", i) is not None]
-
-
-scrape_ASOS()
