19cae005b645e984ce62b33ff14bb4d4054ca299
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-10-16 14:14:51 -0400

    getting started with Consumer_Pages_to_HTML.py

--- Training_Data/python_files/Consumer_Pages_to_HTML.py
+++ Training_Data/python_files/Consumer_Pages_to_HTML.py
@@ -28,3 +28,24 @@ def scrape_ASOS():
     a_tags_list = [asos_as_txt[_.span()[0]:_.span()[1]] for _ in a_tags]
     WM_page_list = [i for i in a_tags_list if re.search("id=\"women-floor\">WOMEN<", i) is not None or
                     re.search("id=\"men-floor\">MEN<", i) is not None]
+
+
+def scrape_lowes():
+    print(f"{Warning_Text}This function has not been completed: scrape_lowes()")
+    with open("../html_files/lowes.html", "r") as lowes_file:
+        lowes_as_txt = lowes_file.read()
+        a_tags = re.finditer("<a.*?>.*?</a>", lowes_as_txt)
+
+    a_tags_list = [lowes_as_txt[_.span()[0]:_.span()[1]] for _ in a_tags]
+    # for _ in a_tags_list:
+    #     print(f"\033[0m{_}")
+    SA_list = [i for i in a_tags_list if re.search("href=\".*?\"", i)]
+    # for _ in SA_list:
+    #     print(f"\033[0m{_}")
+    # IL --> individual link
+    IL_list = [re.search("href=\".*?\"", i) for i in SA_list]
+    IL_list = [i.group() for i in IL_list]
+    print(IL_list)
+
+
+scrape_lowes()
