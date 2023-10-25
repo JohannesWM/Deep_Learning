c98fc9b56b9fbf84844963b89e5d5af6690512d8
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-10-25 18:12:43 -0400

    Finishing to scrape bed bath and body

--- Training_Data/python_files/Website_to_HTML.py
+++ Training_Data/python_files/Website_to_HTML.py
@@ -4,9 +4,7 @@ import re
 
 def WebToHTML(website_link, file_name=None, header=None):
     if header is None:
-        header = {
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko)'
-                          'SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36'}
+        header = {"User-Agent": "Opera/9.80 (X11; Linux x86_64; U; de) Presto/2.2.15 Version/10.00"}
     content = requests.get(website_link, headers=header).text
 
     with open(file_name, "w") as new_file:
@@ -26,3 +24,6 @@ def get_from_list():
                 WebToHTML(_.replace("\n", ""), f"{name}.html")
             except Exception as e:
                 print(e)
+
+
+WebToHTML("https://www.asos.com/us/", file_name="../html_files/ASOS/asos.html")
